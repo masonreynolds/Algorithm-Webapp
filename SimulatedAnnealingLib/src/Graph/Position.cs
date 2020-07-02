620dfe0b0874e998577988da398db12001bec71e
Jacob Krevat <jacobtie@gmail.com>
2020-07-02 18:44:15 -0400

    Added remove pos

--- SimulatedAnnealingLib/src/Graph/Position.cs
+++ SimulatedAnnealingLib/src/Graph/Position.cs
@@ -3,38 +3,48 @@ using System;
 
 namespace Graph
 {
-    public class Position
-    {
-        static private char currName = 'A';
+	public class Position
+	{
+		static private char currName = 'A';
 
-        [Required]
-        [Range(-90, 90)]
-        public double lat { get; set; }
-        [Required]
-        [Range(-180, 180)]
-        public double lon { get; set; }
-        [Required]
-        public string name { get; set; }
+		[Required]
+		public int ID { get; set; }
+		[Required]
+		[Range(-90, 90)]
+		public double lat { get; set; }
+		[Required]
+		[Range(-180, 180)]
+		public double lon { get; set; }
+		[Required]
+		public string name { get; set; }
 
-        public Position()
-        {
-            this.name = currName.ToString();
-            currName++;
-        }
+		public Position()
+		{
+			this.name = currName.ToString();
+			currName++;
+		}
 
-        public Position(double lat, double lon)
-        {
-            this.name = currName.ToString();
-            this.lat = lat;
-            this.lon = lon;
-            currName++;
-        }
+		public Position(double lat, double lon)
+		{
+			this.name = currName.ToString();
+			this.lat = lat;
+			this.lon = lon;
+			currName++;
+		}
 
-        public Position(string name, double lat, double lon)
-        {
-            this.name = name;
-            this.lat = lat;
-            this.lon = lon;
-        }
-    }
+		public Position(string name, double lat, double lon)
+		{
+			this.name = name;
+			this.lat = lat;
+			this.lon = lon;
+		}
+
+		public Position(int iD, double lat, double lon, string name)
+		{
+			ID = iD;
+			this.lat = lat;
+			this.lon = lon;
+			this.name = name;
+		}
+	}
 }
